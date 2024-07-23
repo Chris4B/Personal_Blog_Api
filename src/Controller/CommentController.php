c6b22691adc124ea15ee8b078bd62a87bfe31eeb
Chris Balla <ballachris13@gmail.com>
2024-07-23 13:34:22 +0200

    get all comments and commentbyid

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CommentController extends AbstractController
 {
-    #[Route('/comment', name: 'app_comment')]
-    public function index(): JsonResponse
+    #[Route('api/comments', name: 'get_all_comments')]
+    public function getAllComments(CommentRepository $commentRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CommentController.php',
-        ]);
+        $comments = $commentRepository->findAll();
+
+        $jsonComments = $serializer->serialize($comments,'json', ['groups'=>['comment:read']]);
+
+        return new JsonResponse($jsonComments, Response::HTTP_OK,[], true);
+    }
+
+    #[Route('api/comments/{id}', name:'get_comment_by_id')]
+    public function getCommentById(Comment $comment, SerializerInterface $serializer): JsonResponse
+    {
+        if($comment){
+            $jsonComments = $serializer->serialize($comment,'json', ['Groups'=>['comment:read']]);
+
+            return new JsonResponse($jsonComments);
+        }
+
+        return new JsonResponse(['error' => 'No Comment Found'], Response::HTTP_NOT_FOUND);
     }
 }
